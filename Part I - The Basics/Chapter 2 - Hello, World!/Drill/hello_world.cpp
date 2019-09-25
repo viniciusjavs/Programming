c8cbc87cbf4a4107aa89ea439d5f7692993eaaf7
Vinicius Sa <viniciusjavs@gmail.com>
2019-09-25 18:57:50 -0300

    Add Chapter 2 Drill.

---
+++ Part I - The Basics/Chapter 2 - Hello, World!/Drill/hello_world.cpp
@@ -0,0 +1,8 @@
+#include "std_lib_facilities.h"
+
+int main() // C++ programs starts by executing the function main
+{
+  cout << "Hello, World!\n"; // output "Hello, World!"
+  keep_window_open(); // wait for a character to be entered
+  return 0;
+}
