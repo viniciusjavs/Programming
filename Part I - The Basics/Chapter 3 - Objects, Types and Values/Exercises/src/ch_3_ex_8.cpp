83b2e96231e10fb6915e4f36d1e49fb7fd381488
Vinicius Sa <viniciusjavs@gmail.com>
2019-10-16 19:16:42 -0300

    Use Bjarne Stroustrup approach.
    Chapter 3, Exercise 8.

--- Part I - The Basics/Chapter 3 - Objects, Types and Values/Exercises/src/ch_3_ex_8.cpp
+++ Part I - The Basics/Chapter 3 - Objects, Types and Values/Exercises/src/ch_3_ex_8.cpp
@@ -5,6 +5,7 @@
 
 /*
   Program to test if a number is odd or even.
+  Bjarne Stroustrup technique - pick a default value and change it case needed.
 */
 
 #include "std_lib_facilities.h"
@@ -13,10 +14,10 @@ int main()
 {
     cout << "Enter a integer number :\n";
     int num;
+    string res;
     while (cin >> num) {
-        if (num % 2 == 0)
-            cout << "The number " << num << " is odd\n";
-        else
-            cout << "The number " << num << " is even\n";
+        res = "even";
+        if (num % 2 == 0) res = "odd";
+        cout << "The number " << num << " is " << res << "\n";
     }
 }
